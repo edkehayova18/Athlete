32c90d8fc09314a315cf616029e0b5ca7e915249
edkehayova18 <edkehayova18@codingburgas.bg>
2022-11-30 15:29:53 +0200

    Initial commit

---
+++ src/main/java/Athlete.java
@@ -0,0 +1,15 @@
+public class Athlete {
+    private String lasName;
+
+    public Athlete() {
+
+    }
+
+    public String getLasName() {
+        return lasName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lasName = lasName;
+    }
+}
---
+++ src/main/java/controller/AthleteController.java
@@ -0,0 +1,16 @@
+package controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/player")
+
+public class AthleteController {
+
+  
+
+
+
+}
